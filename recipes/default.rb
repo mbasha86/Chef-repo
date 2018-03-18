591a71035f2e00c5c506e9253d9f63e0e6be93a7
root <root@localhost>
2018-03-17 23:00:21 -0400

    add default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,7 @@
 # Cookbook:: learn_chef
 # Recipe:: default
 #
-# Copyright:: 2018, The Authors, All Rights Reserved.
+# Copyright:: 2018, The Authors, All Rights Reserved
+service 'httpd' do
+	action [:stop]
+end
